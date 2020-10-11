3902fa35e4d2c31d581d8d8c61340c1bc8d40e65
Phuc Vuu <phucvv.hv@gmail.com>
2020-10-11 11:06:49 +0700

    Init beer order client

---
+++ src/main/java/com/microservice/beerorderclient/BeerOrderClientApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.beerorderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerOrderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerOrderClientApplication.class, args);
+    }
+
+}
